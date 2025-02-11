56f492bc0f7e09eef8cff83cd9c00bc9e85dded6
Sami <samikshajagne2@gmail.com>
2025-02-12 01:52:30 +0530

    after adding -e .

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+# from typing import List
+
+def get_requirements() -> list[str]:
+    requirements_list: list[str] = []
+    # You can add your logic to populate the requirements_list here
+    return requirements_list
+
+setup(
+    name='sensor',
+    version="0.0.1",
+    author="Samiksha",
+    author_email='samikshajagne2@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements()  # ["pymongo"]
+)
